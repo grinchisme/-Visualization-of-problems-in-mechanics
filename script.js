f39e0580fe5c4799c1e4888825c9c2451f859045
grinchisme <demyanov0808@gmail.com>
2022-09-27 20:16:35 +0700

    add new details

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+
+const canvas2 = document.getElementById("canvas2");
+const context2 = canvas2.getContext("2d");
 
 canvas.width =800;
 canvas.height =600;
+
+canvas2.width =800;
+canvas2.height =600;
 
 const rm = 100;
 const Rb = 1.5 * rm;
@@ -33,80 +39,156 @@ const h = 0.5 * rm;
 // context.stroke();
 
 // Приступим к ее анимированию
-requestAnimationFrame(tick);
-let pTimestamp = 0;
 let angle = 0;
 let angle2 = 0;
 
-function tick(timestamp){
+animation({
+    clear () {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    },
+
+    update () {
+        angle -= Math.PI * 0.01;
+        angle2 -= Math.PI * 0.02;
+    },
+
+    render () {
+        context.strokeStyle = "black";
+    
+        context.beginPath();
+        context.arc(canvas.width / 2, canvas.height / 2, Rb, 0, Math.PI * 2);
+        context.stroke();
+    
+        context.beginPath();
+        context.arc(canvas.width / 2, canvas.height / 2, 2, 0, Math.PI * 2);
+        context.stroke();
+    
+        context.beginPath();
+        context.arc(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle), 
+            rm,
+            0,
+            Math.PI * 2
+        );
+        context.stroke();
+    
+        context.beginPath();
+        context.arc(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle), 
+            2,
+            0,
+            Math.PI * 2
+        );
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(canvas.width / 2, canvas.height / 2);
+        context.lineTo(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle),
+        );
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle),
+        );
+        context.lineTo(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle) + h* Math.cos(angle2),
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle) + h* Math.sin(angle2)
+        );
+        context.stroke();
+    
+        //Строим систему координат
+        context.beginPath();
+        context.moveTo(canvas.width / 2, 0);
+        context.lineTo(canvas.width / 2, 600);
+        context.stroke();
+        
+        context.font = "15px sans-serif";
+        context.fillText("y", canvas.width / 2 - 20, 15)
+    
+        context.beginPath();
+        context.moveTo(0, canvas.height / 2);
+        context.lineTo(800, canvas.height / 2);
+        context.stroke();
+    
+        context.fillText("x", 800 -20 , canvas.height / 2 + 20)
+    
+        context.beginPath();
+        context.moveTo(canvas.width / 2, 0);
+        context.lineTo(canvas.width / 2 + 5, 10);
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(canvas.width / 2, 0);
+        context.lineTo(canvas.width / 2 - 5, 10);
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(800, canvas.height / 2);
+        context.lineTo(800 - 10, canvas.height / 2 - 5);
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(800, canvas.height / 2);
+        context.lineTo(800 -10, canvas.height / 2 + 5);
+        context.stroke();
+    
+        //Строим точку М
+        context.strokeStyle = "green";
+    
+        context.beginPath();
+        context.arc(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle) + h* Math.cos(angle2),
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle) + h* Math.sin(angle2),
+            2,
+            0,
+            Math.PI * 2
+        );
+        context.stroke();
+
+        context2.strokeStyle = "green";
+
+        context2.beginPath();
+        context2.arc(
+            (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle) + h* Math.cos(angle2),
+            (canvas.height / 2) + (Rb-rm) * Math.sin(angle) + h* Math.sin(angle2),
+            1,
+            0,
+            Math.PI * 2
+        );
+        context2.stroke();
+    }
+})
+
+function animation(obj){
+    const {clear, update, render} = obj;
+    let pTimestamp = 0;
+
     requestAnimationFrame(tick);
-    
-    const diff = timestamp - pTimestamp;
-    pTimestamp = timestamp;
 
-    angle += Math.PI * 0.01;
-    angle2 += Math.PI * 0.02;
+    function tick(timestamp){
+        requestAnimationFrame(tick);
+        
+        const diff = timestamp - pTimestamp;
+        pTimestamp = timestamp;
+        const fps = 1000 / diff;
+        const secondPart = diff / 1000;
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.strokeStyle = "black";
-    
-    context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, Rb, 0, Math.PI * 2);
-    context.stroke();
+        const params = {
+            timestamp,
+            pTimestamp,
+            diff,
+            fps,
+            secondPart
+        }
 
-    context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, 2, 0, Math.PI * 2);
-    context.stroke();
-
-    context.beginPath();
-    context.arc(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle), 
-        rm,
-        0,
-        Math.PI * 2
-    );
-    context.stroke();
-
-    context.beginPath();
-    context.arc(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle), 
-        2,
-        0,
-        Math.PI * 2
-    );
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(canvas.width / 2, canvas.height / 2);
-    context.lineTo(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle),
-    );
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle), 
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle),
-    );
-    context.lineTo(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle) + h* Math.cos(angle2),
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle) + h* Math.sin(angle2)
-    );
-    context.stroke();
-
-    context.strokeStyle = "green";
-
-    context.beginPath();
-    context.arc(
-        (canvas.width / 2 ) + (Rb-rm) * Math.cos(angle) + h* Math.cos(angle2),
-        (canvas.height / 2) + (Rb-rm) * Math.sin(angle) + h* Math.sin(angle2),
-        2,
-        0,
-        Math.PI * 2
-    );
-    context.stroke();
+        update(params);
+        clear();
+        render(params);
+    }
 }
-
